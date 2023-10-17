e466f7bae157402bc5e6bb2b29688f6874664558
ahuactzin <juan_manuel_ahuactzin@msn.com>
2023-10-16 20:15:15 -0600

    Adding a new endpoint

--- router/main.py
+++ router/main.py
@@ -5,9 +5,18 @@ app = FastAPI()
 class HelloWorld():
     def read_hello(self):
         return {"data": "Hello World2"}
+    
+    def read_by(self):
+        return {"data": "By By"}
+    
 router = APIRouter()
+
 router.add_api_route('/api/v2/hello-world', 
-endpoint = HelloWorld().read_hello, methods=["GET"])
+                     endpoint = HelloWorld().read_hello, methods=["GET"])
+
+router.add_api_route('/api/v2/by', 
+                     endpoint = HelloWorld().read_by, methods=["GET"])
+
 app.include_router(router)
 
 if __name__ == "__main__":
