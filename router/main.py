aa191f0a989955e4c6c106db644afe9059eb7ae6
ahuactzin <juan_manuel_ahuactzin@msn.com>
2023-10-16 20:41:59 -0600

    Comment code

--- router/main.py
+++ router/main.py
@@ -2,27 +2,27 @@ from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 import uvicorn
 
-class PromocashData(BaseModel):
-    Plazo: str
-    Categoria: str
-    Unidad: str
-    Edad_Solicitud: int
-    Genero: str 
-    Estado_Civil:str
-    Estudios: str
-    Fuente_Ingresos: str	
-    CP: int
-    Anos_Residencia: int
-    Tipo_Vivienda: str
-    Asentamiento: str
-    Antiguedad: int
-    Ingresos: int
-    Egresos: int
-    Ingreso_Conyuge: str
-    CP_Aval: int
-    Monto: int
-    Score_Agt: float
-    Processed: str | None = None
+# class PromocashData(BaseModel):
+#     Plazo: str
+#     Categoria: str
+#     Unidad: str
+#     Edad_Solicitud: int
+#     Genero: str 
+#     Estado_Civil:str
+#     Estudios: str
+#     Fuente_Ingresos: str	
+#     CP: int
+#     Anos_Residencia: int
+#     Tipo_Vivienda: str
+#     Asentamiento: str
+#     Antiguedad: int
+#     Ingresos: int
+#     Egresos: int
+#     Ingreso_Conyuge: str
+#     CP_Aval: int
+#     Monto: int
+#     Score_Agt: float
+#     Processed: str | None = None
 
 app = FastAPI()
 class HelloWorld():
